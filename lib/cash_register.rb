2ac5c1607c5ae17d8e7c9d322488755ed0612e54
Rathi <github email address>
2017-06-14 14:07:21 -0400

    did this self understanding lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+class CashRegister
 
+attr_accessor :total, :discount, :previous_total
+
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title,price,quantity=1)
+    self.total += (price * quantity)
+    quantity.times do
+      @items << title
+    end
+    self.previous_total = price * quantity
+  end
+
+  def apply_discount
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+    "There is no discount to apply."
+  end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    self.total = self.total - self.previous_total
+  end
+end
